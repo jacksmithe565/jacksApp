fe25e78a8859c2a4bf1326d25d02595873e4be1b
jacksmithe565 <147956682+jacksmithe565@users.noreply.github.com>
2025-07-12 03:19:04 +0200

    Create blockieIdenticon.component.js

---
+++ ui/components/ui/identicon/blockieIdenticon/blockieIdenticon.component.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { blo } from 'blo';
+
+const BlockieIdenticon = ({ address, diameter, alt = '', borderRadius }) => (
+  <img src={blo(address)} height={diameter} width={diameter} style={{ borderRadius }} alt={alt} />
+);
+
+BlockieIdenticon.propTypes = {
+  address: PropTypes.string.isRequired,
+  diameter: PropTypes.number.isRequired,
+  alt: PropTypes.string,
+  borderRadius: PropTypes.string,
+};
+
+export default BlockieIdenticon;
